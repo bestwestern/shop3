9cd18864859108aff2510f99dd313e25bffa65e4
bestwestern <runjep@gmail.com>
2023-11-12 12:19:50 +0100

    jkl

--- src/components/Counter.tsx
+++ src/components/Counter.tsx
@@ -4,13 +4,13 @@ import './Counter.css';
 export default function Counter({ children, count }) {
 	const add = () => count.value++;
 	const subtract = () => count.value--;
-
+const click=()=>{
+	console.log("JKl")
+}
 	return (
 		<>
 			<div class="counter">
-				<button onClick={subtract}>-</button>
-				<pre>{count}</pre>
-				<button onClick={add}>+</button>
+				<button onClick={click}>klik</button>
 			</div>
 			<div class="counter-message">{children}</div>
 		</>
